0806c8cf1cb7bb218471a0c065c90afe9c858187
Aaryan Porwal <aaryanporwal2233@gmail.com>
2021-12-18 17:49:25 +0530

    App works now! 😊✨

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 #!/usr/bin/env node
 
-const puppeteer = require('puppeteer');
-const readline = require('readline');
+const puppeteer = require("puppeteer");
+let url = process.argv[2];
+if (!url) {
+  // throw "Please provide URL as a first argument";
+  url =
+    "https://www.youtube.com/playlist?list=PL4cUxeGkcC9i9Ae2D9Ee1RvylH38dKuET";
+}
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+(async function calculateTotalTime() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "networkidle0" });
 
-let nums = []
-// get the url from the user input
-rl.question('Enter the url: ', (url) => {}
-  // async function configureBrowser() {
-  //   const browser = await puppeteer.launch();
-  //   const page = await browser.newPage();
-  //   await page.goto(url);
-  //   return page;
-  // }
+  let playlistLength = await page.evaluate(async () => {
+    // const delay = ms => new Promise(res => setTimeout(res, ms));
+    // await delay(100); // delay for loading the whole page (100ms)
+    let el = document.querySelectorAll("span#text");
+    let totalTime = 0;
+    let floats = [];
+    el.forEach((e) => {
+      let e1 = e.innerText.replace(":", ".");
+      floats.push(parseFloat(e1));
+    });
+    floats.forEach((float) => {
+      totalTime += float;
+    });
+    return totalTime;
+  });
+  let hours = Math.floor(playlistLength / 60);
+  let minutes = Math.floor(playlistLength % 60);
 
-  // async function getVideoTime(page) {}
-  // rl.close();
+  console.log(`Total playlist time:
+    ${playlistLength}
+    ${hours} hours and ${minutes} minutes
+  `);
+  await browser.close();
+})();
